96b446bd396884ca608c9c69bcfe498340ed6a8f
zeppelin27 <danteteruk@gmail.com>
2014-11-24 18:28:36 +0200

    make vievs for club, change clubscontroller, translate navigation bar

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,2 +1,43 @@
 class ClubsController < ApplicationController
+
+def new
+	@club = Club.new
 end
+
+def index
+	@club = Club.all
+end
+
+def show
+	@club = Club.find(params[:id])
+end
+
+def edit
+	@club = Club.find(params[:id])
+end
+
+def create
+    @club = Club.new(club_params)
+
+    @club.save
+    redirect_to @club
+ end
+
+  def update
+    @club = Club.find(params[:id])
+ 
+    if @club.update(club_params)
+      redirect_to @club
+    else
+      render 'edit'
+    end
+  end
+
+
+private
+    def club_params
+      params.require(:club).permit(:name, :address, :about, :photo_id, :forsquare_id)
+    end
+
+end
+
