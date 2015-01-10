cf8be581584a0f03a8329624f748fe0caf956104
= <=>
2015-01-10 13:11:06 +0200

    first step

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,10 +34,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-     
-                                                                  }
-          config.action_mailer.smtp_settings = {
+   config.action_mailer.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
   :port      => 587,
   :user_name => 'danteteruk@gmail.com',
